0f82adaddace3353b45f03f1236b814f3441d088
Shivansh_Kumar <91822395+creator5112021@users.noreply.github.com>
2022-03-30 17:37:41 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,5 @@
+var classname =  document.getElementById("class").value
+var section =  document.getElementById("section").value
+
+var full_class = [classname, section];
+//So the full class is the name of the class that will be displayed on the classes div.
